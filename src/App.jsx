de4330147605c411ea020ba92350973754f2264a
rahad404 <rahad.0911@gmail.com>
2026-04-01 20:14:45 +0600

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import Banner from "./components/Banner"
 import NavBar from "./components/NavBar"
 
 function App() {
-
+  const data = {
+    title: "My App",
+    links: [
+      { name: "Home", url: "/" },
+      { name: "About", url: "/about" },
+      { name: "Contact", url: "/contact" }
+    ]
+  }
   return (
     <>
-      <NavBar />
+      <NavBar data={data}/>
+      <Banner />
     </>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,37 @@
 const NavBar = () => {
-  return (
-    <nav className="navbar">
-      <h1>My App</h1>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
+   let cartCount = 3; // Example cart count, you can replace this with actual state or props
+   return (
+      <div className="navbar">
+         <div className="navbar-start">
+            <div className="flex items-center gap-1 font-bold text-3xl
+        bg-gradient-to-r from-indigo-500 to-purple-500 text-transparent bg-clip-text">
+               DigiTools
+            </div>
+         </div>
+         <div className="navbar-center hidden md:flex">
+            <ul className="menu menu-horizontal gap-10 px-1 ">
+               <li>
+                  <a>Product</a>
+               </li>
+               <li>
+                  <a>feature</a>
+               </li>
+               <li>
+                  <a>Pricing</a>
+               </li>
+               <li>
+                  <a>Testimonial</a>
+               </li>
+               <li>
+                  <a>FAQ</a>
+               </li>
+            </ul>
+         </div>
+         <div className="navbar-end gap-5">
+            <a className="btn bg-gradient-to-r from-indigo-500 to-purple-500 rounded-full text-white">Get Started</a>
+         </div>
+      </div>
+   );
 };
 
 export default NavBar;
