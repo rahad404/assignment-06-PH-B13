4ad89f18bf6579d80a3709e752b5b7202cf77825
rahad404 <rahad.0911@gmail.com>
2026-04-01 18:57:06 +0600

    initial commit with all setup

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My App</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
